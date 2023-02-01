e2898c39d5d25a13530713f9010fbaef9180b3c8
Cédric GOUJON <cedgoujon78@gmail.com>
2023-02-01 15:12:24 +0100

    DataFixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,13 +2,24 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\Product;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        for ($i = 1; $i < 50; $i++) {
+            $product = new Product();
+            $product->setName('Saucisse ' . $i);
+            $product->setSlug('product-' . $i);
+            $product->setDescription('Description ' . $i);
+            $product->setPrice('10.00');
+            $product->setImage('saussage.jpg');
+            $manager->persist($product);
+        }
+        
         // $product = new Product();
         // $manager->persist($product);
 
